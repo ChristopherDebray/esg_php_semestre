3b9a39af6508238c285da84ce2ea6e05991988b2
Christopher Debray <christopher.debray@club-employes.com>
2023-06-12 22:41:26 +0200

    ADD basic implementation of css framework

--- www/views/account.php
+++ www/views/account.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <title>Mon site MVC</title>
     <meta name="description" content="mon super site en MVC from scratch">
+    <link rel="stylesheet" href="../dist/main.css">
 </head>
 <body>
 <h1>Comptes</h1>
-
 
 <!-- la vue  -->
 <?php include $this->view; ?>
