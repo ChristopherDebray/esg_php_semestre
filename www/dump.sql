6afc80852a136377c572d242f3e89c3fcf41ff24
Fansoa <fverin.contactpro@gmail.com>
2023-06-13 12:29:37 +0200

    feat: create esgi_page table

--- www/dump.sql
+++ www/dump.sql
@@ -25,4 +25,18 @@ INSERT INTO "public"."esgi_user" (firstname, lastname, email, pwd, date_inserted
 INSERT INTO "public"."esgi_user" (firstname, lastname, email, pwd, date_inserted, date_updated, status, role) VALUES ('Anne', 'Dupond', 'annedupond@gmail.com', '$2y$10$mLKjj7saZwKJ73EiGIAJd.5S5b0ciFsPR4mplK469x1n77VjIei4i', '2023-06-13 09:30:38', '2023-06-13 09:30:38', 1, 'guest');
 INSERT INTO "public"."esgi_user" (firstname, lastname, email, pwd, date_inserted, date_updated, status, role) VALUES ('Patrick', 'Verger', 'patrickverger@gmail.com', '$2y$10$mLKjj7saZwKJ73EiGIAJd.5S5b0ciFsPR4mplK469x1n77VjIei4i', '2023-06-13 09:30:38', '2023-06-13 09:30:38', 1, 'guest');
 
+CREATE TABLE "public"."esgi_page" (
+                                    "id" integer DEFAULT nextval('esgi_page_id_seq') NOT NULL,
+                                    "user_id" integer,
+                                    CONSTRAINT fk_page_user_id FOREIGN KEY(user_id) REFERENCES esgi_user(id),
+                                    "title" character varying(60) NOT NULL,
+                                    "slug" character varying(60) NOT NULL,
+                                    "date_inserted" timestamp NOT NULL,
+                                    "date_updated" timestamp NOT NULL,
+                                    "config" jsonb NOT NULL,
+                                    "content" jsonb NOT NULL,
+                                    "theme" integer NOT NULL,
+                                    "status" integer NOT NULL
+);
+
 -- 2023-04-18 16:57:56.748799+00
