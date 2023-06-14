f1266a167831e87d1b3f8ec6c05c4be0a047e0ea
Fansoa <fverin.contactpro@gmail.com>
2023-06-14 16:25:40 +0200

    feat: add hard & soft delete user

--- www/controllers/Admin.php
+++ www/controllers/Admin.php
@@ -11,7 +11,38 @@ final class Admin {
         $users = $user->getAll();
         $view=new View("admin/dashboard");
         $view->assign('data', $users);
+    }
 
+    public function hardDeleteUser()
+    {
+        $id = $_GET['id'];
+        if (!empty($id)) {
+            $user = new User();
+            $user = $user->getOneBy(['id' => $id]);
+            $user->setFirstname('firstname');
+            $user->setLastname('lastname');
+            $user->setEmail('email@mail.com');
+            $user->setIsDeleted(1);
+            $user = $user->save();
+        }
+        else
+        {
+            die("Pas d'ID retourné");
+        }
+    }
 
+    public function softDeleteUser()
+    {
+        $id = $_GET['id'];
+        if (!empty($id)) {
+            $user = new User();
+            $user = $user->getOneBy(['id' => $id]);
+            $user->setIsDeleted(1);
+            $user = $user->save();
+        }
+        else
+        {
+            die("Pas d'ID retourné");
+        }
     }
 }
