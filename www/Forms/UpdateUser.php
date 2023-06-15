c26ff1ba30eb719f6ee5eac5869015e9ad4436e1
Fansoa <fverin.contactpro@gmail.com>
2023-06-15 14:55:28 +0200

    feat: update UpdateUser

--- www/Forms/UpdateUser.php
+++ www/Forms/UpdateUser.php
@@ -6,18 +6,31 @@ use App\core\Validator;
 class UpdateUser extends Validator {
     public $config = [];
     public $method = "POST";
+    public $statusOptions = [
+        ["label"=>"STATUS_INACTIVE", "value"=>0],
+        ["label"=>"STATUS_ACTIVE", "value"=>1],
+        ["label"=>"STATUS_BANNED", "value"=>2]
+    ];
+    public $roleOptions = [
+        ["label"=>"admin", "value"=>"admin"],
+        ["label"=>"suscriber", "value"=>"suscriber"],
+        ["label"=>"guest", "value"=>"guest"]
+    ];
+    public $user = null;
 
     public function __construct($user)
     {
-        var_dump($user->getFirstname());
+        $this->user=$user;
         $this->config = [
             "config"=>[
                 "method"=>$this->method,
+                "title"=>"Modifier l'utilisateur",
                 "action"=>"",
-                "class"=>null,
+                "class"=>"col-6",
                 "id"=>"form-update-user",
                 "submit"=>"Modifier l'utilisateur",
-                "cancel"=>"Annuler"
+                "cancel"=>"Annuler",
+                "redirectIfCancel"=>"dashboard"
             ],
             "inputs"=>[
                 "firstname"=>[
@@ -29,18 +42,68 @@ class UpdateUser extends Validator {
                     "error"=>"Prénom incorrect",
                     "value"=>$user->getFirstname()
                 ],
+                "lastname"=>[
+                    "type"=>"text",
+                    "placeholder"=>"Nom",
+                    "required"=>true,
+                    "id"=>"lastname",
+                    "class"=>null,
+                    "error"=>"Nom incorrect",
+                    "value"=>$user->getLastname()
+                ],
                 "email"=>[
                     "type"=>"email",
                     "placeholder"=>"Votre email",
                     "required"=>true,
                     "id"=>"input-email",
                     "class"=>null,
-                    "error"=>"Votre email est incorrect"
+                    "error"=>"Votre email est incorrect",
+                    "value"=>$user->getEmail()
+                ],
+                "status"=>[
+                    "type"=>"select",
+                    "options"=>$this->statusOptions,
+                    "id"=>"status",
+                    "name"=>"status",
+                    "value"=>$this->getStatusDefaultValue(),
+                    "label"=>$this->getStatusDefaultLabel(),
+                ],
+                "role"=>[
+                    "type"=>"select",
+                    "options"=>$this->roleOptions,
+                    "id"=>"role",
+                    "name"=>"role",
+                    "label"=>$this->getRoleDefaultLabel(),
+                    "value"=>$this->getRoleDefaultValue()
                 ],
             ]
         ];
 
         parent::__construct();
+    }
+
+    public function getStatusDefaultValue()
+    {
+        $statusDefaultValue = array_search($this->user->getStatus(), array_column($this->statusOptions, 'value'));
+        return $this->statusOptions[$statusDefaultValue]["value"];
+    }
+
+    public function getStatusDefaultLabel()
+    {
+        $statusDefaultLabel = array_search($this->user->getStatus(), array_column($this->statusOptions, 'label'));
+        return $this->statusOptions[$statusDefaultLabel]["label"];
+    }
+
+    public function getRoleDefaultValue()
+    {
+        $roleDefaultValue = array_search($this->user->getRole(), array_column($this->roleOptions, 'value'));
+        return $this->roleOptions[$roleDefaultValue]["value"];
+    }
+
+    public function getRoleDefaultLabel()
+    {
+        $roleDefaultValue = array_search($this->user->getRole(), array_column($this->roleOptions, 'value'));
+        return $this->roleOptions[$roleDefaultValue]["label"];
     }
 
     public function getConfig(): array
--- www/views/admin/updateUser.php
+++ www/views/admin/updateUser.php
@@ -1,4 +1,1 @@
-<h2 class="container pb-5">Modifier l'utilisateur</h2>
-
-
 <?php $this->modal("form", $form, $formErrors);?>
