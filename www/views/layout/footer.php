5b160242927fdec2526db97ba164b0a211ac8593
Fansoa <fverin.contactpro@gmail.com>
2023-06-16 18:05:01 +0200

    fix: add footer tag with class row

--- www/views/layout/footer.php
+++ www/views/layout/footer.php
@@ -1,5 +1,7 @@
-<div class="footer col-12" style="background-color: <?= $dataFooter["footerColor"] ?>">
-    <p>
-        <?= $dataFooter["companyTitle"] ?>©
-    </p>
-</div>
+<footer class="row">
+    <div class="footer col-12" style="background-color: <?= $dataFooter["footerColor"] ?>">
+        <p>
+            <?= $dataFooter["companyTitle"] ?>©
+        </p>
+    </div>
+</footer>
