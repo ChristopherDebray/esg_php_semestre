b28154811c6c1563c6ba1c536941245394ded8e3
Fansoa <fverin.contactpro@gmail.com>
2023-06-16 18:37:39 +0200

    feat: add post component

--- www/controllers/DesignTestController.php
+++ www/controllers/DesignTestController.php
@@ -32,6 +32,22 @@ final class DesignTestController {
         "footerColor"=>"#f6b73c"
     ];
 
+    public $dataPost = [
+        ["imgSrc"=>"https://placehold.co/400x400",
+        "imgAlt"=>"Description of the picture",
+        "title"=>"Heading",
+        "text"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
+        ["imgSrc"=>"https://placehold.co/400x400",
+        "imgAlt"=>"Description of the picture",
+        "title"=>"Heading",
+        "text"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
+        ["imgSrc"=>"https://placehold.co/400x400",
+        "imgAlt"=>"Description of the picture",
+        "title"=>"Heading",
+        "text"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
+
+    ];
+
     public function displayWireframe1()
     {
         $view = new View('wireframe1', 'front');
@@ -40,6 +56,16 @@ final class DesignTestController {
         $view->assign('dataSlideshow', $this->dataSlideshow);
         $view->assign('dataCustomcard', $this->dataCustomCard);
         $view->assign('dataQuote', $this->dataQuote);
+        $view->assign('dataPost', $this->dataPost);
+        
+    }
+
+    public function displayWireframe2()
+    {
+        $view = new View('wireframe2', 'front');
+        $view->assign('dataFooter', $this->dataFooter);
+        $view->assign('dataBanner', $this->dataBanner);
+        $view->assign('dataPost', $this->dataPost);
         
     }
 }
---
+++ www/views/wireframe2.php
@@ -0,0 +1,6 @@
+<?php include 'layout/header.php' ?>
+
+<?php include 'components/post.php' ?>
+
+<?php include 'layout/footer.php' ?>
+
