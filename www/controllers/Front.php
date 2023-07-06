d13ac5b7a60e5aa13e585ecba8b120c4521fe475
LeGrizzly <xalsie.ff@hotmail.fr>
2023-07-06 10:04:03 +0200

    ADD Generate sitemap dynamically

--- www/controllers/Front.php
+++ www/controllers/Front.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\controllers;
+
 use App\core\View;
+use App\core\ConnectDB;
 
 final class Front{
-
-
     public function home()
     {
         $pseudo = "Prof";
@@ -13,11 +13,36 @@ final class Front{
         $view->assign("lastname", "SKZYPCZYK");
     }
 
-
     public function contact()
     {
         die("Voici ma nouvelle page de contact");
     }
 
+    public function sitemap()
+    {
+        $data = [];
+        $urls = [];
 
+        $connectDb = ConnectDB::getInstance();
+        $queryPrepared = $connectDb->getPdo()->prepare('SELECT slug FROM '.DB_PREFIX.'page');
+        $queryPrepared->execute();
+        $queryPrepared->setFetchMode(\PDO::FETCH_ASSOC);
+        $objet = $queryPrepared->fetchAll();
+
+        foreach ($objet as $r) {
+            foreach ($r as $key => $value) {
+                $data[] = $value;
+            }
+        }
+
+        $_yml = yaml_parse_file("./routes.yml");
+
+        foreach ($_yml as $key => $value) {
+            if (!isset($value['permission'])) $urls[] = $key;
+        }
+
+        $allUrls = array_merge($urls, $data);
+        $view = new View('main/sitemap', 'xml');
+        $view->assign('data', $allUrls);
+    }
 }
